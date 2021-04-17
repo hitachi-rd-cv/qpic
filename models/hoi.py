a94a43bc0e56bbfddd1dad8d050b021ba546f8d8
Masato Tamura <masato.tamura.sf@hitachi.com>
2021-04-17 13:01:22 +0000

    [fix] remove neg_weights

--- models/hoi.py
+++ models/hoi.py
@@ -175,12 +175,10 @@ class SetCriterionHOI(nn.Module):
         pos_inds = gt.eq(1).float()
         neg_inds = gt.lt(1).float()
 
-        neg_weights = torch.pow(1 - gt, 4)
-
         loss = 0
 
         pos_loss = torch.log(pred) * torch.pow(1 - pred, 2) * pos_inds
-        neg_loss = torch.log(1 - pred) * torch.pow(pred, 2) * neg_weights * neg_inds
+        neg_loss = torch.log(1 - pred) * torch.pow(pred, 2) * neg_inds
 
         num_pos  = pos_inds.float().sum()
         pos_loss = pos_loss.sum()
